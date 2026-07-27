19b7dbde45bb1a28c17647645873c7e09e1c7c44
medinah <ethusiast.medinah@cloudethusiat.com>
2026-07-27 14:57:47 +0100

    new update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,6 +15,7 @@ const Hero = () => {
         <div>
             <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#f7941d]'>Medinat</span></h1>
             <p className={`${styles.heroSubText} text-white-100`}>I Develop Webapp, Search Engine Optimisation,User <br className='sm:block hidden'></br>interfaces and Graphic Illustrations </p>
+            <p></p>
         </div>
       </div>
       <ComputersCanvas/>
